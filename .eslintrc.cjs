0a99600f4b937eee533bbe0cdaf8fff66ef10cc5
정 현 진 <jhj1004v@naver.com>
2024-05-08 01:36:45 +0900

    CHORE : eslint, prettier 세팅

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,6 +5,7 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
@@ -14,5 +15,9 @@ module.exports = {
       'warn',
       { allowConstantExport: true },
     ],
+    //mac window
+    'prettier/prettier': ['error', { endOfLine: 'auto' }],
+    'no-empty': 'warn', // 빈 블록문에 대한 경고 설정
+    '@typescript-eslint/no-unused-vars': 'off', // TypeScript 코드에서 사용되지 않는 변수를 감지하고 경고를 발생하지 않겠다.
   },
 };
