425a4321ae2f4bb9933d2bbcd7f29a8de9e10286
정 현 진 <jhj1004v@naver.com>
2024-05-08 19:00:46 +0900

    DESIGN : 초기세팅 및 폰트설정

--- src/styles/GlobalStyle.tsx
+++ src/styles/GlobalStyle.tsx
@@ -1,11 +1,18 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
+    @font-face {
+        font-family: 'SOGANGUNIVERSITYTTF';
+        src: url('https://fastly.jsdelivr.net/gh/projectnoonnu/noonfonts_2312-1@1.1/SOGANGUNIVERSITYTTF.woff2') format('woff2');
+        font-weight: normal;
+        font-style: normal;
+    }
     * {
         margin:0; padding:0;
     }
     body {
         line-height:1;
+        font-family: 'SOGANGUNIVERSITYTTF';
     }
     img {
         vertical-align:top;
