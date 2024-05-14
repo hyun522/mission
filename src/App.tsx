c5d942bc496f56f579abee3066707e2bb7321cd5
정 현 진 <jhj1004v@naver.com>
2024-05-14 14:18:52 +0900

    FEAT : 페이지 이동 기능

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,37 @@
+import { NavLink } from 'react-router-dom';
 import ToDoList from './components/ToDoList';
+import styled from 'styled-components';
+
+const NavGnb = styled.ul`
+  display: flex;
+`;
+const LoginLi = styled.li`
+  margin-right: 10px;
+`;
+
+function getLinkStyle({ isActive }: { isActive: boolean }) {
+  console.log(isActive);
+  return {
+    textDecoration: isActive ? 'underline' : '',
+    color: '#000',
+  };
+}
 
 export default function App() {
   return (
     <>
+      <NavGnb>
+        <LoginLi>
+          <NavLink to='/signin' style={getLinkStyle}>
+            로그인
+          </NavLink>
+        </LoginLi>
+        <li>
+          <NavLink to='/signup' style={getLinkStyle}>
+            회원가입
+          </NavLink>
+        </li>
+      </NavGnb>
       <ToDoList />
     </>
   );
