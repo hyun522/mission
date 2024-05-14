b6cf3ca854c0c042fd201932a531e2ffe3625426
정 현 진 <jhj1004v@naver.com>
2024-05-14 16:28:19 +0900

    DESIGN :  signup UI 작업

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,3 +1,79 @@
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+const Bg = styled.div`
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const SignUpBox = styled.div`
+  width: 30dvw;
+  text-align: center;
+`;
+
+const SignUpTitle = styled.p`
+  width: 25dvw;
+  line-height: 30px;
+  margin: 0 auto;
+  font-size: 20px;
+  margin-bottom: 30px;
+`;
+
+const EmailInput = styled.input`
+  width: 100%;
+  padding: 15px;
+  border-radius: 50px;
+  border: 1px solid #ddd;
+`;
+
+const PassWordInput = styled(EmailInput)`
+  margin: 20px 0 30px 0;
+`;
+
+const GoLogin = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-top: 20px;
+  font-size: 14px;
+  color: #777;
+`;
+
+const NextButton = styled.button`
+  background-color: #ddd;
+  width: 100%;
+  color: #fff;
+  padding: 15px;
+  border-radius: 50px;
+  border: none;
+  cursor: pointer;
+`;
+
+const LinkText = styled(Link)`
+  font-size: 14px;
+  color: #444;
+  margin-left: 5px;
+  text-decoration: underline;
+`;
+
 export default function SignUp() {
-  return <div></div>;
+  return (
+    <Bg>
+      <SignUpBox>
+        <SignUpTitle>
+          회원가입을 하고 모든 서비스를 무료로 이용하세요.
+        </SignUpTitle>
+        <form>
+          <EmailInput placeholder='이메일' />
+          <PassWordInput placeholder='비밀번호' />
+          <NextButton>다음</NextButton>
+        </form>
+        <GoLogin>
+          <p>이미회원이신가요?</p>
+          <LinkText to='/signin'>로그인하기</LinkText>
+        </GoLogin>
+      </SignUpBox>
+    </Bg>
+  );
 }
