ee8b4400571b859972ec6ca7fbdd5e1efc1fac27
정 현 진 <jhj1004v@naver.com>
2024-05-16 04:28:34 +0900

    REMOVE :  header 컴포넌트로 분리

---
+++ src/components/Header.tsx
@@ -0,0 +1,37 @@
+import { NavLink } from 'react-router-dom';
+import styled from 'styled-components';
+
+const NavGnb = styled.ul`
+  display: flex;
+`;
+const LoginLi = styled.li`
+  margin-right: 10px;
+`;
+
+export default function Header() {
+  const storageData = JSON.parse(localStorage.getItem('key') || '{}');
+
+  return (
+    <NavGnb>
+      {storageData ? (
+        <>
+          <LoginLi>
+            <NavLink to='/signin'>{storageData.email}</NavLink>
+          </LoginLi>
+          <li>
+            <NavLink to='/signup'>로그아웃</NavLink>
+          </li>
+        </>
+      ) : (
+        <>
+          <LoginLi>
+            <NavLink to='/signin'>로그인</NavLink>
+          </LoginLi>
+          <li>
+            <NavLink to='/signup'>회원가입</NavLink>
+          </li>
+        </>
+      )}
+    </NavGnb>
+  );
+}
