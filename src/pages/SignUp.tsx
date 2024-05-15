c5ad01fd36a4f3ecb9818eb6edb796daf6a45ec8
정 현 진 <jhj1004v@naver.com>
2024-05-16 02:11:14 +0900

    FEAT :  유효성 검사

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,5 +1,12 @@
+import { useForm, SubmitHandler } from 'react-hook-form';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
+
+interface Inputs {
+  email: string;
+  password: string;
+  checkPassword: string;
+}
 
 const Bg = styled.div`
   height: 100vh;
@@ -10,7 +17,6 @@ const Bg = styled.div`
 
 const SignUpBox = styled.div`
   width: 30dvw;
-  text-align: center;
 `;
 
 const SignUpTitle = styled.p`
@@ -19,6 +25,7 @@ const SignUpTitle = styled.p`
   margin: 0 auto;
   font-size: 20px;
   margin-bottom: 30px;
+  text-align: center;
 `;
 
 const EmailInput = styled.input`
@@ -31,8 +38,17 @@ const EmailInput = styled.input`
 const PassWordInput = styled(EmailInput)`
   margin-top: 20px;
 `;
-const CheckPassWordInput = styled(EmailInput)`
-  margin: 20px 0 30px 0;
+
+const CheckPassWordInput = styled(PassWordInput)``;
+
+const ErrorMessage = styled.div`
+  font-size: 12px;
+  color: #e22424;
+  margin: 5px 0 0 5px;
+`;
+
+const ErrorMessageCheckPassword = styled(ErrorMessage)`
+  margin: 5px 0 15px 5px;
 `;
 
 const GoLogin = styled.div`
@@ -52,6 +68,11 @@ const NextButton = styled.button`
   border: none;
   cursor: pointer;
 `;
+const PasswordValidate = styled.p`
+  font-size: 12px;
+  margin: 15px 0 30px 5px;
+  color: #666;
+`;
 
 const LinkText = styled(Link)`
   font-size: 14px;
@@ -61,17 +82,72 @@ const LinkText = styled(Link)`
 `;
 
 export default function SignUp() {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors, isSubmitting },
+  } = useForm<Inputs>();
+
+  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
+  console.log(watch());
+  console.log(errors);
+
   return (
     <Bg>
       <SignUpBox>
         <SignUpTitle>
           회원가입을 하고 모든 서비스를 무료로 이용하세요.
         </SignUpTitle>
-        <form>
-          <EmailInput placeholder='이메일' />
-          <PassWordInput placeholder='비밀번호' />
-          <CheckPassWordInput placeholder='비밀번호 확인' />
-          <NextButton>다음</NextButton>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <EmailInput
+            placeholder='이메일'
+            {...register('email', {
+              required: true,
+              pattern: /^[^s@]+@[^s@]+\.[^s@]+$/,
+            })}
+          />
+          {errors.email && (
+            <ErrorMessage> 올바른 이메일 주소가 아닙니다. </ErrorMessage>
+          )}
+
+          <PassWordInput
+            placeholder='비밀번호'
+            type='password'
+            {...register('password', {
+              required: true,
+              pattern:
+                /^(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+            })}
+          />
+          {errors.password && (
+            <ErrorMessage>
+              비밀번호는 영문, 숫자 조합 8자 이상 입력해 주세요.
+            </ErrorMessage>
+          )}
+
+          <CheckPassWordInput
+            placeholder='비밀번호 확인'
+            type='password'
+            {...register('checkPassword', {
+              required: true,
+              validate: (value: string) => {
+                if (watch('password') !== value) {
+                  return 'not match';
+                }
+              },
+            })}
+          />
+          {errors.checkPassword && (
+            <ErrorMessageCheckPassword>
+              비밀번호가 일치하지 않습니다.
+            </ErrorMessageCheckPassword>
+          )}
+          <PasswordValidate>
+            ⚠︎ 비밀번호는 영어 소문자, 숫자, 특수문자 포함 8-20자
+          </PasswordValidate>
+
+          <NextButton disabled={isSubmitting}>다음</NextButton>
         </form>
         <GoLogin>
           <p>이미회원이신가요?</p>
