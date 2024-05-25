6911717e41afabbfabdc450961a27234a0d09b72
정 현 진 <jhj1004v@naver.com>
2024-05-25 12:21:16 +0900

    FEAT : 쇼핑몰 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import Header from './components/Header';
-// import ToDoList from './components/ToDoList';
+import { Outlet } from 'react-router-dom';
 
 export default function App() {
   return (
     <>
       <Header />
+      <Outlet />
       {/* <ToDoList /> */}
     </>
   );
