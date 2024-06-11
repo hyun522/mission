6f0dc7978cec86e993a912027add2d95dcd31a60
정 현 진 <jhj1004v@naver.com>
2024-06-11 16:33:30 +0900

    supabase 활용법

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
 import Header from './components/Header';
 import { Outlet } from 'react-router-dom';
+// import { supabase } from './main';
+// import { useEffect } from 'react';
 
 export default function App() {
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     //*치면 모든 칼럼이라는 뜻
+  //     let { data: test, error } = await supabase.from('test').select('*');
+  //     console.log(test);
+  //   };
+  //   fetchData();
+  // }, []);
   return (
     <>
       <Header />
--- src/main.tsx
+++ src/main.tsx
@@ -10,6 +10,12 @@ import Detail from './pages/Detail.tsx';
 import { CartProvider } from './contexts/CartContext.tsx';
 import Cart from './pages/Cart.tsx';
 
+// import { createClient } from '@supabase/supabase-js';
+// const supabaseUrl = 'https://qqghxjixcyspqgtbzcpt.supabase.co';
+// const supabaseKey =
+//   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFxZ2h4aml4Y3lzcHFndGJ6Y3B0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTc4NTI2NTYsImV4cCI6MjAzMzQyODY1Nn0.ahTNdiwIK_dAj4kb0A7lafaaq3OwYHBWGS6GNQ0yiCU';
+// export const supabase = createClient(supabaseUrl, supabaseKey);
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <CartProvider>
