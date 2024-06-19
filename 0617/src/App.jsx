cc1aa72de57fd994c1a73da5b7fe46f58be660cb
정 현 진 <jhj1004v@naver.com>
2024-06-19 17:13:19 +0900

    CHORE : createBrowserRouter 설정

--- 0617/src/App.jsx
+++ 0617/src/App.jsx
@@ -1,10 +1,15 @@
+import Todolist from './page/todolist/index';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Todolist />,
+  },
+]);
+
 function App() {
-  return (
-    <>
-      <div className='gan'>"그래 여기서"</div>
-      <div className='gkdo'>"그래 여기서"</div>
-    </>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
